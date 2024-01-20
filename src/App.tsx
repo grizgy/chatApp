84858129f141e86f4ebd74e317b2bc78efae8cfb
grizgy <e1427646@student.tuwien.ac.at>
2024-01-20 20:01:19 +0200

    sample template

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Chat from './components/chat'
+
+function App() {
+
+  return (
+
+    <div className='parent'>
+
+        <div id='left'>
+          
+        </div>
+
+        <div id='right'>
+          <Chat></Chat>
+        </div>
+     
+    </div>
+    
+  )
+}
+
+export default App
